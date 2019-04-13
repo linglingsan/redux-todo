39bbce5a7e5e83430f9c09a0b07a75986ef0e1eb
linglingsan <zhangchunlin5614@163.com>
2019-04-13 23:47:39 +0800

    init

---
+++ src/components/demo1.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Demo1 extends React.Component {
+    render() {
+        return (
+            <div>
+                demo1
+            </div>
+        );
+    }
+}
+
+export default Demo1;
---
+++ src/todoList.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import Demo from './components/demo1';
+
+class TodoList extends Component {
+    render() {
+        return (
+            <Demo/>
+        );
+    }
+}
+
+export default TodoList;
